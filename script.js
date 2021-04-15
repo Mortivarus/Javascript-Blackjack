fef900de3cab7feee28c7dda54c12a271d2c3815
Mortivarus <58182213+Mortivarus@users.noreply.github.com>
2021-04-15 13:01:09 +0200

    Added GUI function, finalised game logic

--- script.js
+++ script.js
@@ -46,9 +46,21 @@ const Cards = {
     spade_queen: 10
 }
 
+const addCardtoHTML = (card, player) =>{
+    let img = document.createElement("img")
+    img.className = "column" 
+    img.src = `cards/${card}.png`
+    let src = document.getElementById(`${player.playerType}Deck`)
+    src.appendChild(img)
+}
+
+
+
+
 //Create a player class with a deck and pre-selected player type, as well as a predefined array for aces
 const Player = class {
-    constructor(playerType){
+    constructor(name, playerType){
+        this.name = name
         this.deck = []
         this.aces = ["club_1", "diamond_1", "heart_1", "spade_1"]
         this.playerType = playerType
@@ -95,14 +107,20 @@ const Player = class {
     }
 }
 
-let deck = Object.keys(Cards)
+let deck = Object.keys(Cards) //Create a deck array based on the cards object
 
-let player1 = new Player("dealer")
+let player1 = new Player("Player 1", "player") //Make a new player of type "player"
 
-let dealer = new Player()
+let dealer = new Player("the Dealer", "dealer") //Make a new player of type "dealer"
 
-let status = ""
+var status = ""
 
+//Update the shown deck and score
+const updateDeckAndScore = (player) =>{
+    document.getElementById(`${player.playerType}Deck`).innerHTML = player.deck
+    document.getElementById(`${player.playerType}Score`).innerHTML = player.deckScore()
+
+}
 
 //Draw two cards from the deck and add them to the player deck
 const deal = (player) => {
@@ -117,45 +135,60 @@ const deal = (player) => {
     let forRemove = [draw1, draw2]
     deck = deck.filter(item => !forRemove.includes(item))
     player.deck.push(draw1, draw2)
+    
+    if(player.playerType === "player"){
+        updateDeckAndScore(player)
+    } else if(player.playerType === "dealer"){
+        document.getElementById("dealerDeck").innerHTML = player.deck[0]
+    }
+    
+    return console.log(`Cards have been dealt to ${player.name}`)
+
 }
 
-//Draw a card from the deck. If the player type is a player, and the total score is 
+//Check the win/lose conditions
+const checkConditions = (player, dealer) => {
+    if(player.deckScore() > 21){ //If the player's score is more than 21, they lose
+        return status = "lose"
+    } else if(dealer.deckScore() < 16){ //If the player is not out and the dealer still needs to hit, end function
+        return
+    } else if(dealer.deckScore() > 21 || player.deckScore() === 21){ //If the dealer has more than 21 points or the player has 21 points, the player wins
+        return status = "won"
+    } else if(player.deckScore() > dealer.deckScore()){ //If the player has a higher score than the dealer, they win
+        return status = "won"
+    } else{ //If the player has a lower score than the dealer, they lose (remainder category)
+        return status = "lose"
+    }
+}
+
+//Draw a card from the deck. If the player type is a player, and the total score is more than 21, return the 'lose' status
 const hit = (player) => {
     let draw = deck[Math.floor((Math.random()*deck.length))]
     deck = deck.filter(item => item != draw)
     player.deck.push(draw)
 
-    if(player.playerType === "player" && player.deckScore > 21){
-        status = "lose"
-    }
+    checkConditions(player, dealer)
+    updateDeckAndScore(player)
 }
 
-const checkConditions = (player, dealer) => {
-    if(dealer.deckScore < 16){
-        return
-    }
-
-    if(player.deckScore() > 21){
-        return status = "lose"
-    } else if(dealer.deckScore() > 21 || player.deckScore() === 21){
-        return status = "won"
-    }
-
-
-    
-}
-
-
+//Player passes, dealer gets a card until he has 17 or more points
 const pass = () => {
-
     while(dealer.deckScore() < 17){
         hit(dealer)
+        checkConditions(player1, dealer)
     }
-    
-    
-    
-    
-    
+    updateDeckAndScore(dealer)
+    checkConditions(player1, dealer) 
 }
 
+addCardtoHTML("spade_7", player1)
 
+addCardtoHTML("spade_7", player1)
+
+// addCardtoHTML("spade_7", playerCard3)
+
+// addCardtoHTML("spade_7", "playerCard4")
+
+addCardtoHTML("spade_8", dealer)
+
+addCardtoHTML("spade_7", dealer)
